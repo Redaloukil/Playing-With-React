f05f9b3859126742c5c3d499bced4be667c019ec
Reda Loukil <president.alg@gmail.com>
2017-09-06 10:47:45 +0100

    adding new components and display tasks

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import Dolist from './dolist.js';
 
+const task = [
+  {
+      task:"making some skills on react",
+      status:true
+  },
+  {
+      task:"having my dinner",
+      status:false
+  },
+  {
+      task:"making some skills on react-native",
+      status:false
+  }
+]
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        task:task,
+      }
+  }
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>
-        <Dolist/>
+        <Dolist tasks={this.state.task}/>
       </div>
     );
   }
--- src/dolist.js
+++ src/dolist.js
@@ -1,30 +1,26 @@
 import React, { Component } from 'react';
-import Dolistheader from './header'
-
-const task = [
-    {
-        task:"making some skills on react"
-    },
-    {},
-    {}
-]
-
-
-
+import Dolistheader from './header';
+import Taskitem from './taskitem';
 
 class Dolist extends Component {
     constructor(props){
         super(props);
-        this.state = {
-
-        }
+        this.renderTask = this.renderTask.bind(this)
+    }
+    renderTask(){
+        var arr = [];
+        var taskArr = this.props.tasks;
+        taskArr.map((item , i)=>{
+            arr.push();
+        })
+       return arr;
     }
     render(){
         return(
             <div>
-                
                 <table>
                     <Dolistheader/>
+                    {this.renderTask()}
                 </table>
             </div>
         )
